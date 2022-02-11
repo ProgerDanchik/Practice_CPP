9523826956894e4313bee6d93fc0168c2c9bbb85
styleprog <89692175+ProgerDanchik@users.noreply.github.com>
2022-02-11 15:47:06 +0300

    for loop representation

---
+++ cpp_chapter_5/for/forloop.cpp
@@ -0,0 +1,16 @@
+//forloop.cpp -- for loop representation
+#include <iostream>
+
+int main() {
+	using namespace std;
+
+	int i;
+
+	for (i = 0; i < 5; i++) {
+		cout << "C++ knows loops!\n";
+	}
+
+	cout << "C++ knows when to stop.\n";
+
+	return 0;
+}
