f84fb27bd972e5a2b2fbc0efc7fdf3432f558910
styleprog <89692175+ProgerDanchik@users.noreply.github.com>
2022-02-11 19:26:06 +0300

    использование цикла for для строки

---
+++ cpp_chapter_5/for/forstep1.cpp
@@ -0,0 +1,20 @@
+//forstep1.cpp -- использование цикла for для строки
+#include <iostream>
+#include <string>
+
+int main() {
+	using namespace std;
+
+	cout << "Enter a word: ";
+	string word;
+	cin >> word;
+
+	// Отображение символов в обратном порядке
+	for (int i = word.size() - 1; i >= 0; i--) {
+		cout << word[i];
+	}
+
+	cout << "\nBye\n";
+
+	return 0;
+}
