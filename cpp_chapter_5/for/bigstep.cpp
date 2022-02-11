e181285edcd84d04a4d64dd0cd1532ee1bce357a
styleprog <89692175+ProgerDanchik@users.noreply.github.com>
2022-02-11 19:09:33 +0300

    цикл указанным пользователем шагом

---
+++ cpp_chapter_5/for/bigstep.cpp
@@ -0,0 +1,20 @@
+//bigstep.cpp -- цикл указанным пользователем шагом
+#include <iostream>
+
+int main() {
+	using std::cout;
+	using std::cin;
+	using std::endl;
+
+	cout << "Enter an integer: ";
+	int by;
+	cin >> by;
+
+	cout << "Counting by " << by << "s:\n";
+
+	for (int i = 0; i < 100; i = i + by) {
+		cout << i << endl;
+	}
+
+	return 0;
+}
