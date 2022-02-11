b04e4a3e4930e8130b41380b5b963685228df8e9
styleprog <89692175+ProgerDanchik@users.noreply.github.com>
2022-02-11 21:33:59 +0300

    сравнение строк с использованием массивов

---
+++ cpp_chapter_5/for/compstr1.cpp
@@ -0,0 +1,18 @@
+//compstr1.cpp -- сравнение строк с использованием массивов
+#include <iostream>
+#include <cstring>
+
+int main() {
+	using namespace std;
+
+	char word[5] = "?ate";
+
+	for (char ch = 'a'; strcmp(word, "mate"); ch++) {
+		cout << word << endl;
+		word[0] = ch;
+	}
+
+	cout << "After loop ends, word is " << word << endl;
+
+	return 0;
+}
