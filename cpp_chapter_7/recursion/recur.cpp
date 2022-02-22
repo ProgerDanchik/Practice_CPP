caccd72c86e584daa2f817c6c360bd220e8921fa
styleprog <89692175+ProgerDanchik@users.noreply.github.com>
2022-02-22 11:49:31 +0300

    использование рекурсии

---
+++ cpp_chapter_7/recursion/recur.cpp
@@ -0,0 +1,22 @@
+// recur.cpp -- использование рекурсии
+#include <iostream>
+
+void countdown(int n);
+
+int main() {
+	countdown(4);			// вызов рекурсивной функции
+	
+	return 0;
+}
+
+void countdown(int n) {
+	using namespace std;
+
+	cout << "Counting down... " << n << endl;
+
+	if (n > 0) {
+		countdown(n - 1);		// функция вызывает сама себя
+	}
+
+	cout << n << ": Kaboom!\n";
+}
