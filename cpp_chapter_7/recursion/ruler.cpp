2ac3631da9f86c7069105afb9b7911500e8754a3
styleprog <89692175+ProgerDanchik@users.noreply.github.com>
2022-02-22 11:58:35 +0300

    использование рекурсии для разделения линейки

---
+++ cpp_chapter_7/recursion/ruler.cpp
@@ -0,0 +1,49 @@
+// ruler.cpp -- использование рекурсии для разделения линейки
+#include <iostream>
+using namespace std;
+
+const int LEN = 66;
+const int DIVS = 6;
+
+void subdivide(char ar[], int low, int high, int level);
+
+int main() {
+	char ruler[LEN];
+
+	int i;
+
+	for (i = 1; i < LEN - 2; i++) {
+		ruler[i] = ' ';
+	}
+	
+	ruler[LEN - 1] = '\0';
+	int max = LEN - 2;
+	int min = 0;
+
+	ruler[min] = ruler[max] = '|';
+
+	cout << ruler << endl;
+
+	for (i = 1; i <= DIVS; i++) {
+		subdivide(ruler, min, max, i);
+		cout << ruler << endl;
+
+		for (int j = 1; j < LEN - 2; j++) {
+			ruler[j] = ' ';			//очистка линейки
+		}
+	}
+
+	return 0;
+}
+
+void subdivide(char ar[], int low, int high, int level) {
+	if (level == 0) {
+		return;
+	}
+
+	int mid = (high + low) / 2;
+	ar[mid] = '|';
+
+	subdivide(ar, low, mid, level - 1);
+	subdivide(ar, mid, high, level - 1);
+}
